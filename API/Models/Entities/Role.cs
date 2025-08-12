e4614234fb5344faff4cfc35d2fe2b921f9c098a
kgni <admin@mdia.dk>
2025-08-12 21:18:45 +0200

    modify entities

--- API/Models/Entities/Role.cs
+++ API/Models/Entities/Role.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Models.Entities;
 
-public class Role
+public class Role : Entity<int>
 {
-    
+    [MaxLength(50)] public string Name { get; set; } = "Customer";
+
+    public List<User> Users { get; set; } = new();
+}
+
+public static class RoleNames
+{
+    public const string Admin = "Admin";
+    public const string Receptionist = "Receptionist";
+    public const string Cleaner = "Cleaner";
+    public const string Customer = "Customer";
+
+    public static readonly string[] All = [Admin, Receptionist, Cleaner, Customer];
 }
--- API/Models/Entities/Room.cs
+++ API/Models/Entities/Room.cs
@@ -1,19 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Models.Entities;
 
 [Index(nameof(Number), IsUnique = true)]
-public class Room : Common
+public class Room : Entity<Guid>
 {
-    public required string Number { get; set; }
-    public required short Capacity { get; set; }
+    [StringLength(32)] public required string Number { get; init; }
+    public required short Capacity { get; init; }
     public required decimal PricePerNight { get; set; }
     public required RoomType Type { get; set; } = RoomType.Standard;
-    public short? Floor { get; set; }
+    public short? Floor { get; init; }
     public string? Description { get; set; }
     public bool isActive { get; set; } = false;
 
-    public ICollection<Booking> Bookings { get; set; }
+    public ICollection<Booking> Bookings { get; init; } = new List<Booking>();
 }
 
 public enum RoomType
