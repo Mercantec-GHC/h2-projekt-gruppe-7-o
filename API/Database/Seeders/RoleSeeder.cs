080e4c8ce06a1f7b464a782e4812afd2258628d3
kgni <admin@mdia.dk>
2025-08-12 21:19:23 +0200

    create first seeder - RoleSeeder

---
+++ API/Database/Seeders/RoleSeeder.cs
@@ -0,0 +1,25 @@
+using API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.Seeders;
+
+public class RoleSeeder
+{
+    static public void Seed(DbContext context)
+    {
+        foreach (var roleName in RoleNames.All)
+        {
+            var roleExists = context.Set<Role>().Any(r => r.Name == roleName);
+            if (!roleExists)
+            {
+                context.Set<Role>().Add(new Role
+                {
+                    Name = roleName,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
