b7517764867562ac6f29d17c4d5ec851b72bd07d
kgni <admin@mdia.dk>
2025-08-13 08:47:56 +0200

    add todo

--- API/Models/Entities/Utils/Entity.cs
+++ API/Models/Entities/Utils/Entity.cs
@@ -8,6 +8,7 @@ public interface IAuditable
 
 public abstract class Entity<TKey> : IAuditable
 {
+    //TODO: add soft delete
     public TKey Id { get; protected init; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
