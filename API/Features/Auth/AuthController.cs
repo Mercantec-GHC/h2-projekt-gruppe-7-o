0cc38dd980d1b5f7cb71551447af51f18b012034
kgni <admin@mdia.dk>
2025-09-13 16:12:37 +0200

    Update JWT service and claims configuration
    Include user name claims and use correct hash algorithm

--- API/Features/Auth/AuthController.cs
+++ API/Features/Auth/AuthController.cs
@@ -91,7 +91,7 @@ public class AuthController : ControllerBase
         await _context.SaveChangesAsync();
 
         var token = jwtService.GenerateToken(user);
-        //TODO: maybe we want to return more than just the token 
+        //TODO: maybe we want to return more than just the token
         return Ok(token);
     }
 }
