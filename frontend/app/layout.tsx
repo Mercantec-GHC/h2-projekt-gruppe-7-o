5874ba5bf4bf716cc3335dc0034ef00b495e45a5
kgni <admin@mdia.dk>
2025-10-08 15:04:58 +0200

    move globals.css file out of app folder (caused issues when deploying

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import "@/styles/gradients.css";
 import QueryProvider from "@/providers/QueryProvider";
 import { cn } from "@/lib/utils";
